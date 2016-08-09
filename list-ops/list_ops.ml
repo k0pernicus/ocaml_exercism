d35d5adf047d21a812cb799d73f7eb9bafdaf952
k0pernicus <antonin.carette@gmail.com>
2016-08-08 20:42:14 -0400

    Usage of fold

--- list-ops/list_ops.ml
+++ list-ops/list_ops.ml
@@ -1,51 +1,22 @@
-let length list =
-    let rec aux acc list =
-        match list with
-        | [] -> acc
-        | _ :: t -> aux (acc + 1) t in
-    aux 0 list;;
-
-let reverse list =
-    let rec aux acc list =
-        match list with
-        | [] -> acc
-        | h :: t -> aux (h :: acc) t in
-    aux [] list;;
-
-let map ~f list =
-    let rec aux acc list =
-        match list with
-        | [] -> reverse acc
-        | h :: t -> aux ((f h) :: acc) t in
-    aux [] list;;
-
-let filter ~f list =
-    let rec aux acc list =
-        match list with
-        | [] -> reverse acc
-        | h :: t -> (
-            match (f h) with
-            | true -> aux (h :: acc) t
-            | _ -> aux acc t
-        ) in
-    aux [] list;;
-
 let rec fold ~init:acc ~f l =
     match l with
     | [] -> acc
     | h :: t -> fold ~init:(f acc h) ~f t;;
 
-let append list1 list2 =
-    let rec aux list list2 =
-        match list2 with
-        | [] -> reverse list
-        | h :: t -> aux (h :: list) t in
-    aux (reverse list1) list2;;
+let length list =
+    fold ~init:0 ~f:(fun acc _ -> acc + 1) list;;
 
-let concat listOfLists =
-    let rec aux acc list =
-        match list with
-        | [] -> acc
-        | h :: t -> aux (append acc h) t in
-    aux [] listOfLists;;
+let reverse list =
+    fold ~init:[] ~f:(fun acc h -> h :: acc) list;;
 
+let map ~f list =
+    fold ~init:[] ~f:(fun acc h -> ((f h) :: acc)) (reverse list);;
+
+let append xs ys =
+    fold ~init:ys ~f:(fun acc h -> h :: acc) (reverse xs);;
+
+let concat xss =
+    fold ~init:[] ~f:(fun acc h -> append acc h) xss;;
+
+let filter ~f list =
+    fold ~init:[] ~f:(fun acc h -> if (f h) then (h :: acc) else acc) (reverse list);;
